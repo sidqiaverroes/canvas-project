12cf52bb3099f7511d16326c1a54d0b00657dff5
Sidqi Averroes <sidqi.averroes26@gmail.com>
2025-03-07 09:56:44 +0700

    Sprite class

--- src/main.js
+++ src/main.js
@@ -1,19 +1,47 @@
-import { resources } from './Resource';
 import './style.css'
+import { resources } from './Resource';
+import { Sprite } from './Sprite';
+import { Vector2 } from './Vector2';
 
 const canvas = document.querySelector("#game-canvas");
 const ctx = canvas.getContext("2d");
 
-const draw  = () => {
-  const sky = resources.images.sky;
-  if(sky.isLoaded) {
-    ctx.drawImage(sky.image, 0, 0)
-  }
+const skySprite = new Sprite({
+  resource: resources.images.sky, 
+  frameSize: new Vector2(320, 180),
+})
 
-  const ground = resources.images.ground;
-  if(ground.isLoaded) {
-    ctx.drawImage(ground.image, 0, 0)
-  }
+const groundSprite = new Sprite({
+  resource: resources.images.ground, 
+  frameSize: new Vector2(320, 180),
+})
+
+const hero = new Sprite({
+  resource: resources.images.hero,
+  frameSize: new Vector2(32, 32),
+  hFrames: 3,
+  vFrames: 8,
+  frame: 1,
+})
+
+const shadow = new Sprite({
+  resource: resources.images.shadow, 
+  frameSize: new Vector2(32, 32),
+})
+
+const heroPos = new Vector2(16*6, 16*5);
+
+const draw  = () => {
+  skySprite.drawImage(ctx, 0, 0);
+  groundSprite.drawImage(ctx, 0, 0);
+
+  //center the hero in the cell
+  const heroOffset = new Vector2(-8, -21);
+  const heroPosX = heroPos.x + heroOffset.x;
+  const heroPosY = heroPos.y + heroOffset.y;
+
+  shadow.drawImage(ctx, heroPosX, heroPosY);
+  hero.drawImage(ctx, heroPosX, heroPosY);
 }
 
 setInterval(() => {
